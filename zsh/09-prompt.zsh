51213b14e57ada469108c5b46766f00fea19f277
crshd <christian.brassat@gmail.com>
2013-09-05 18:25:58 +0800

    i really am

--- zsh/09-prompt.zsh
+++ zsh/09-prompt.zsh
@@ -17,6 +17,7 @@ precmd() {
 	vcs_info
 	
 	# check if virtualenv activated and get the name
+	# Yes, I really am too cool to use $(basedir)
 	[ $VIRTUAL_ENV ] && local venv="%F{10}(venv:%f${VIRTUAL_ENV##*/}%F{10})%f "
 	
 	typeset -A items

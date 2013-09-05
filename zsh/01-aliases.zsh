94abd1bfad57b946454051ca4dd62bd1a6ac85fd
crshd <christian.brassat@gmail.com>
2013-09-05 17:15:57 +0800

    more minor changes

--- zsh/01-aliases.zsh
+++ zsh/01-aliases.zsh
@@ -5,6 +5,7 @@
 # aka.: y u no conform to XDG?
 alias mplayer="$(command -v mplayer2 || echo "mplayer") -noconfig user -include $XDG_CONFIG_HOME/mplayer/mplayer.conf"
 alias rtorrent="rtorrent -n -o import=$XDG_CONFIG_HOME/rtorrent/rtorrent.rc"
+alias ncmpcpp="ncmpcpp -c $XDG_CONFIG_HOME/ncmpcpp/ncmpcpp.conf"
 
 # Recurse, goddamnit!
 alias cp="cp -r"
@@ -23,10 +24,7 @@ alias gp="git push"
 alias gco="git config"
 
 # wrapper scripts
-alias tmux="tmux_wrapper.sh"
+alias t="tmux_wrapper.sh"
 
 # randoms
 alias rs="(redshift -l 3:101 -b0.7 -t 5500:4700&)"
-
-# Office
-alias word="WINEPREFIX=$XDG_DATA_HOME/wine/32 wine ~/var/wine/32/drive_c/Program\ Files/Microsoft\ Office/Office12/WINWORD.EXE"

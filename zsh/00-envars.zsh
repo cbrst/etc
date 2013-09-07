53130d0d8f1d03d945414a11285e5066de2dbaa6
crshd <christian.brassat@gmail.com>
2013-09-07 18:52:51 +0800

    add node and games to $PATH

--- zsh/00-envars.zsh
+++ zsh/00-envars.zsh
@@ -3,6 +3,6 @@ export XDG_DATA_HOME=$HOME/var
 
 export BIN_HOME=$HOME/bin
 
-export PATH=$BIN_HOME:$BIN_HOME/scripts:/usr/local/bin:/usr/bin:/bin:/opt/bin:/usr/x86_64-pc-linux-gnu/gcc-bin/4.6.4
+export PATH=$BIN_HOME:$BIN_HOME/scripts:$HOME/node/bin:/usr/local/bin:/usr/bin:/bin:/opt/bin:/usr/x86_64-pc-linux-gnu/gcc-bin/4.6.4:/usr/games/bin
 
 

080774005460dc68894f520af14295d96a6e6f37
crshd <christian.brassat@gmail.com>
2013-09-08 15:15:59 +0800

    add xinit

--- zsh/00-envars.zsh
+++ zsh/00-envars.zsh
@@ -6,6 +6,9 @@ export BIN_HOME=$HOME/bin
 export PATH=$BIN_HOME:$BIN_HOME/scripts:$HOME/node/bin:/usr/local/bin:/usr/bin:/bin:/opt/bin:/usr/x86_64-pc-linux-gnu/gcc-bin/4.6.4:/usr/games/bin
 
 # adjust for $XDG_CONFIG_HOME
+
 export VIMPERATOR_RUNTIME=$XDG_CONFIG_HOME/vimperator
 export MY_VIMPERATORRC=$VIMPERATOR_RUNTIME/vimperator.rc
 export VIMPERATOR_INIT=":source $MY_VIMPERATORRC"
+
+export XINITRC=$XDG_CONFIG_HOME/X11/xinitrc

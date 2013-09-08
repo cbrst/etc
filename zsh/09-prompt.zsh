3f174a1dfa8a31e32e07738e3a212e62f73d67a6
crshd <christian.brassat@gmail.com>
2013-09-08 17:35:12 +0800

    more cleanup

--- zsh/09-prompt.zsh
+++ zsh/09-prompt.zsh
@@ -23,31 +23,23 @@ zstyle ':vcs_info:git:*' actionformats   "%F{9}(${FMT_REPO}%F{9}) ${FMT_BRANCH} 
 zstyle ':vcs_info:hg:*'  actionformats   "%F{13}(${FMT_REPO}%F{13}) ${FMT_BRANCH} %u%c ${FMT_ACTION}"
 zstyle ':vcs_info:svn:*' actionformats   "%F{14}(${FMT_REPO}%F{14}) ${FMT_BRANCH} %u%c ${FMT_ACTION}"
 
+local PS_USER="%(#.%F{1}.%F{3})%n%f"
+local PS_MACH="%F{1}%M%f"
+local PS_PWD="%F{5}%~%f"
+local PS_TTY="%F{4}%y%f"
+local PS_EXIT="%(?..%F{202}%?%f )"
+
 precmd() {
-	
+	# Enable VCS display
 	vcs_info
+	RPROMPT=${vcs_info_msg_0_}
 	
 	# check if virtualenv activated and get the name
 	# Yes, I really am too cool to use $(basename)
-	[ $VIRTUAL_ENV ] && local venv="%F{10}(venv:%f%B${VIRTUAL_ENV##*/}%b%F{10})%f %B$(python -V 2>&1 | cut -d ' ' -f 2)%b "
-	
-	typeset -A items
-	items=(
-		user    '%(#.%F{1}.%F{3})%n'
-		machine '%F{1}%M'
-		pwd     '%F{5}%~'
-		tty     '%F{4}%y'
-		exit    '%(?..%F{202}%?%f)'
-		vcs     "${vcs_info_msg_0_}"
-		venv    "$venv"
-	)
+	[ $VIRTUAL_ENV ] && local PS_VENV="%F{10}(venv:%f%B${VIRTUAL_ENV##*/}%b%F{10})%f %B$(python -V 2>&1 | cut -d ' ' -f 2)%b"
 
-	show=(venv pwd)
-
-	PS1=' '
-	for item in $show; do
-		PS1+="${items[${item}]}%f"
-	done
-	PS1+=" %F{11}»%f "
-	RPROMPT=${vcs_info_msg_0_}
+	# Set prompt
+	PS1="${PS_EXIT}${PS_VENV} ${PS_PWD} %F{11}»%f "
 }
+	
+

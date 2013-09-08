1ba3d9c9e4331f3d076cdb07040af3c28bc787d8
crshd <christian.brassat@gmail.com>
2013-09-08 14:23:03 +0800

    toggle wrap

--- emacs/user/keybindings.el
+++ emacs/user/keybindings.el
@@ -3,3 +3,6 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; toggle word wrap
+(global-set-key (kbd "<f5>") 'toggle-word-wrap)

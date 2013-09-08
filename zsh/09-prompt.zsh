c5da7097db892b98060f51ebd42d00cb025ac664
crshd <christian.brassat@gmail.com>
2013-09-08 16:48:22 +0800

    playing with vcs_info

--- zsh/09-prompt.zsh
+++ zsh/09-prompt.zsh
@@ -1,16 +1,24 @@
 autoload -U vcs_info && vcs_info
 
-local FMT_BRANCH="%F{9}(%s:%F{7}%{$IT%}%r%{$ST%}%F{9}) %F{11}%B%b %K{235}%{$IT%}%u%c%{$ST%}%k"
+# I really have no idea why there isn't a zstyle for this
 local FMT_ACTION="(%F{3}%a%f)"
-local FMT_PATH="%F{1}%R%F{2}/%S%f"
+
+# I would have liked to use branchformat, but not all backends support that.
+local FMT_BRANCH="%F{11}%B%b%%b%f"
 
 zstyle ':vcs_info:*' enable git hg svn
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr   "%F{62}D%F{237}IRTY%f"
-zstyle ':vcs_info:*' stagedstr     "%F{62}S%F{237}TAGED"
-zstyle ':vcs_info:*' actionformats "${FMT_BRANCH}${FMT_ACTION}" "${FMT_PATH}"
-zstyle ':vcs_info:*' formats       "${FMT_BRANCH}"              "${FMT_PATH}"
-zstyle ':vcs_info:*' nvcsformats   ""                           "%~"
+
+zstyle ':vcs_info:*' unstagedstr         "%K{235}%F{62}D%F{237}IRTY%f%k"
+zstyle ':vcs_info:*' stagedstr           "%K{235}%F{62}S%F{237}TAGED%f%k"
+
+zstyle ':vcs_info:git:*' formats         "%F{9}(%s:%F{7}%B%r%F{9}) ${FMT_BRANCH} %u%c"
+zstyle ':vcs_info:hg:*'  formats         "%F{13}(%s:%F{7}%B%r%F{13}) ${FMT_BRANCH} %u%c"
+zstyle ':vcs_info:svn:*' formats         "%F{14}(%s:%F{7}%B%r%F{14}) ${FMT_BRANCH} %u%c"
+
+zstyle ':vcs_info:git:*' actionformats   "%F{9}(%s:%F{7}%B%r%F{9}) ${FMT_BRANCH} %u%c ${FMT_ACTION}"
+zstyle ':vcs_info:hg:*'  actionformats   "%F{13}(%s:%F{7}%B%r%F{13}) ${FMT_BRANCH} %u%c ${FMT_ACTION}"
+zstyle ':vcs_info:svn:*' actionformats   "%F{14}(%s:%F{7}%B%r%F{14}) ${FMT_BRANCH} %u%c ${FMT_ACTION}"
 
 precmd() {
 	
@@ -18,7 +26,7 @@ precmd() {
 	
 	# check if virtualenv activated and get the name
 	# Yes, I really am too cool to use $(basename)
-	[ $VIRTUAL_ENV ] && local venv="%F{10}(venv:%f${VIRTUAL_ENV##*/}%F{10})%f "
+	[ $VIRTUAL_ENV ] && local venv="%F{10}(venv:%f%B${VIRTUAL_ENV##*/}%b%F{10})%f %B$(python -V 2>&1 | cut -d ' ' -f 2)%b "
 	
 	typeset -A items
 	items=(

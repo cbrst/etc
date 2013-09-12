a520e2dc1d8f7b7f245a5b1f94281e8200ce1289
crshd <christian.brassat@gmail.com>
2013-09-12 23:12:38 +0800

    screenshot functions

--- zsh/02-functions.zsh
+++ zsh/02-functions.zsh
@@ -3,3 +3,7 @@ xfont() { xrdb -query | grep font | head -1 | cut -f 2 }
 
 # cd with ls
 cdl() { cd $1; ls }
+
+# take, upload and delete scrot
+tud() { scrot ${1}.png; fb ${1}.png; rm ${1}.png }
+tuds() { scrot -s ${1}.png; fb ${1}.png; rm ${1}.png }

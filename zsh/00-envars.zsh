aec73f72109b4384c6a99e6bf63474c9e8f78d47
crshd <christian.brassat@gmail.com>
2013-09-17 15:52:33 +0800

    Cleaner /home/crshd/bin:/home/crshd/bin/scripts:/home/crshd/node/bin:/usr/local/bin:/usr/bin:/bin:/opt/bin:/usr/x86_64-pc-linux-gnu/gcc-bin/4.6.4:/usr/games/bin. I seriously love ZSH.

--- zsh/00-envars.zsh
+++ zsh/00-envars.zsh
@@ -3,7 +3,18 @@ export XDG_DATA_HOME=$HOME/var
 
 export BIN_HOME=$HOME/bin
 
-export PATH=$BIN_HOME:$BIN_HOME/scripts:$HOME/node/bin:/usr/local/bin:/usr/bin:/bin:/opt/bin:/usr/x86_64-pc-linux-gnu/gcc-bin/4.6.4:/usr/games/bin
+path_dirs=(
+	${BIN_HOME}
+	${BIN_HOME}/scripts
+	${XDG_DATA_HOME}/node/bin
+	/usr/local/bin
+	/usr/bin
+	/bin
+	/opt/bin
+	/usr/x86_64-pc-linux-gnu/gcc-bin/4.6.4
+	/usr/games/bin
+)
+export PATH=${(j_:_)path_dirs}
 
 # adjust for $XDG_CONFIG_HOME
 

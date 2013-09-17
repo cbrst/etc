9a7bea8710f18090eec98ff754a1e72af7855820
crshd <christian.brassat@gmail.com>
2013-09-17 15:27:17 +0800

    misc keybinds

--- emacs/user/keybindings.el
+++ emacs/user/keybindings.el
@@ -4,5 +4,15 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-;; toggle word wrap
-(global-set-key (kbd "<f5>") 'toggle-word-wrap)
+;; toggle truncate lines
+(global-set-key (kbd "<f5>") 'toggle-truncate-lines)
+
+;; i don't use fillcolumn
+(global-set-key (kbd "C-x f") 'ido-find-file)
+
+;; M-x is a pain to reach. at least on QWERTZ
+(global-set-key (kbd "C-x x") 'execute-extended-command)
+
+;; evil
+(define-key evil-normal-state-map (kbd "gg") 'evilnc-comment-or-uncomment-lines)
+(define-key evil-visual-state-map (kbd "gg") 'evilnc-comment-or-uncomment-lines)

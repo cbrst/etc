68a890015ccc629e47da8dbbad7497686b8f6207
crshd <christian.brassat@gmail.com>
2013-09-25 15:01:25 +0800

    autocomplete emmet

--- emacs/user/filetypes/ft-html.el
+++ emacs/user/filetypes/ft-html.el
@@ -4,3 +4,7 @@
 (mmm-add-mode-ext-class 'html-erb-mode nil 'html-css)
 
 (add-to-list 'auto-mode-alist '(".ejs" . html-erb-mode))
+
+;; autocomplete-emmet
+(add-hook 'sgml-mode-hook 'ac-emmet-html-setup)
+(add-hook 'css-mode-hook 'ac-emmet-css-setup)
